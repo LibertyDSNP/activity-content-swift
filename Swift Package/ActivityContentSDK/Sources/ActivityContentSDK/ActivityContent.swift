73583a8117ea6fa7ef154339684aef0daf92455e
Rigo Carbajal <rigo.carbajal@unfinished.com>
2022-01-05 17:13:05 -0800

    Expanding Example support for Location

--- Swift Package/ActivityContentSDK/Sources/ActivityContentSDK/ActivityContent.swift
+++ Swift Package/ActivityContentSDK/Sources/ActivityContentSDK/ActivityContent.swift
@@ -10,20 +10,37 @@ import Foundation
 public class ActivityContent {
     
     public static func createNote(content: String,
-                                  mediaType: String) -> Note? {
-
-        return Note(content: "This is a note",
-                        mediaType: "text/plain",
-                        name: "Sample Name",
-                        published: Date(timeIntervalSince1970: 1640321788.6924329),
-                        attachment: [
-                            ImageAttachment(url: [ImageLink(href: URL(string: "http://www.example.com")!, mediaType: "image/png", hash: [Hash(algorithm: "keecak", value: HashUtil.hash(content: "Lorem Ipsum")!)], height: 400, width: 400)], name: "Image Name"),
-                            VideoAttachment(url: [VideoLink(href: URL(string: "http://www.example.com")!, mediaType: "video/mp4", hash: [Hash(algorithm: "keecak", value: HashUtil.hash(content: "Lorem Ipsum")!)], height: 400, width: 400)], name: "Video Name", duration: 30),
-                            AudioAttachment(url: [AudioLink(href: URL(string: "http://www.example.com")!, mediaType: "video/mp4", hash: [Hash(algorithm: "keecak", value: HashUtil.hash(content: "Lorem Ipsum")!)])], name: "Audio Name", duration: 30),
-                            LinkAttachment(href: URL(string: "http://www.example.com")!, name: "Link Name")
-                        ],
-                        tag: [Hashtag(name: "#hashtag"), Mention(name: "Mention Name", id: "dsnp://user")],
-                        location: Location(name: "Location Name", accuracy: 50, altitude: 25, latitude: 123.23, longitude: -45.234, radius: 100, units: .cm))
+                                  mediaType: String,
+                                  name: String? = nil,
+                                  published: Date? = nil,
+                                  attachment: [BaseAttachment]? = nil,
+                                  tag: [BaseTag]? = nil,
+                                  location: Location? = nil) -> Note? {
+        
+        return Note(content: content,
+                    mediaType: mediaType,
+                    name: name,
+                    published: published,
+                    attachment: attachment,
+                    tag: tag,
+                    location: location)
+    }
+    
+    public static func createLocation(name: String,
+                                      accuracy: Float? = nil,
+                                      altitude: Float? = nil,
+                                      latitude: Float? = nil,
+                                      longitude: Float? = nil,
+                                      radius: Float? = nil,
+                                      units: LocationUnits? = nil) -> Location? {
+        
+        return Location(name: name,
+                        accuracy: accuracy,
+                        altitude: altitude,
+                        latitude: latitude,
+                        longitude: longitude,
+                        radius: radius,
+                        units: units)
     }
     
 }
--- Swift Package/Example/Example/ViewController.swift
+++ Swift Package/Example/Example/ViewController.swift
@@ -9,14 +9,26 @@ import UIKit
 import ActivityContentSDK
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let note = ActivityContent.createNote(content: "This is content", mediaType: "text/plain")
-        print(note!)
+        let location = ActivityContent.createLocation(
+            name: "Location",
+            accuracy: 50,
+            altitude: 100,
+            latitude: 123.45,
+            longitude: -123.45,
+            radius: 25,
+            units: .feet)
+        
+        _ = ActivityContent.createNote(
+            content: "This is a note",
+            mediaType: "text/plain",
+            name: "Sample Name",
+            published: Date(timeIntervalSince1970: 1640321788.6924329),
+            attachment: nil,
+            tag: nil,
+            location: location)
     }
-
-
 }
-

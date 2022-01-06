4c2c6d1d88a35313abcf4c4fe4392d1ba16b6a48
Rigo Carbajal <rigo.carbajal@unfinished.com>
2022-01-06 14:33:31 -0800

    Tiny rename on Test file.

---
+++ Swift Package/ActivityContentSDK/Tests/ActivityContentSDKTests/Source/ActivityContentTests.swift
@@ -0,0 +1,12 @@
+//
+//  ActivityContentTests.swift
+//  ActivityContentTests
+//
+//  Created by Unfinished on 12/22/21.
+//
+
+import XCTest
+@testable import ActivityContentSDK
+
+class ActivityContentTests: XCTestCase {   
+}

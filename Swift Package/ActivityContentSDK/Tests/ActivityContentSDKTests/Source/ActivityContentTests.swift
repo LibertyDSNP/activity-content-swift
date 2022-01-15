ac3edff70c4620f692c6b8fc4a97e76d23d29592
Rigo Carbajal <rigo.carbajal@unfinished.com>
2022-01-14 16:42:25 -0800

    Comleted writing tests for Builder classes.git s

--- Swift Package/ActivityContentSDK/Tests/ActivityContentSDKTests/Source/ActivityContentTests.swift
+++ Swift Package/ActivityContentSDK/Tests/ActivityContentSDKTests/Source/ActivityContentTests.swift
@@ -7,30 +7,9 @@
 
 import XCTest
 @testable import ActivityContentSDK
+import CoreLocation
 
 class ActivityContentTests: XCTestCase {
-    
-    func testBuildHashtag() {
-        let object = try? ActivityContent.Builders.Tags.Hashtag()
-            .setName("#hashtag")
-            .addAdditionalFields(["custom" : true])
-            .build()
-        
-        XCTAssertEqual(object?.name, "#hashtag")
-        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
-    }
-    
-    func testBuildMention() {
-        let object = try? ActivityContent.Builders.Tags.Mention()
-            .setName("Mention Name")
-            .setDSNPUserId("dsnp://1234")
-            .addAdditionalFields(["custom" : true])
-            .build()
-        
-        XCTAssertEqual(object?.name, "Mention Name")
-        XCTAssertEqual(object?.id, "dsnp://1234")
-        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
-    }
     
     func testBuildAudioAttachment() {
         let object = try? ActivityContent.Builders.Attachments.Audio()
@@ -51,6 +30,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Audio")
         XCTAssertEqual(object?.name, "Audio Attachment")
         XCTAssertEqual(object?.duration, 180)
         XCTAssertEqual(object?.url?.first?.mediaType, "audio/ogg")
@@ -70,6 +50,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Link")
         XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
         XCTAssertEqual(object?.hash?.first?.algorithm, "keccak")
         XCTAssertEqual(object?.mediaType, "audio/ogg")
@@ -94,6 +75,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Image")
         XCTAssertEqual(object?.name, "Image Attachment")
         XCTAssertEqual(object?.url?.first?.mediaType, "image/png")
         XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
@@ -112,6 +94,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Link")
         XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
         XCTAssertEqual(object?.hash?.first?.algorithm, "keccak")
         XCTAssertEqual(object?.mediaType, "image/png")
@@ -137,6 +120,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Video")
         XCTAssertEqual(object?.name, "Video Attachment")
         XCTAssertEqual(object?.duration, 180)
         XCTAssertEqual(object?.url?.first?.mediaType, "video/H265")
@@ -156,6 +140,7 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Link")
         XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
         XCTAssertEqual(object?.hash?.first?.algorithm, "keccak")
         XCTAssertEqual(object?.mediaType, "video/H265")
@@ -169,8 +154,196 @@ class ActivityContentTests: XCTestCase {
             .addAdditionalFields(["custom" : true])
             .build()
         
+        XCTAssertEqual(object?.type, "Link")
         XCTAssertEqual(object?.name, "Link Attachment")
         XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildHash() {
+        let object = try? ActivityContent.Builders.Hash()
+            .setAlgorithm("keccak")
+            .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.algorithm, "keccak")
+        XCTAssertEqual(object?.value, "0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildLocation() {
+        let object = try? ActivityContent.Builders.Location()
+            .setName("Location Name")
+            .setAccuracy(100)
+            .setAltitude(200)
+            .setCoordinate(CLLocationCoordinate2D(latitude: 123.45, longitude: -123.45))
+            .setRadius(50)
+            .setUnits(.km)
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.type, "Place")
+        XCTAssertEqual(object?.name, "Location Name")
+        XCTAssertEqual(object?.accuracy, 100)
+        XCTAssertEqual(object?.altitude, 200)
+        XCTAssertEqual(object?.latitude, 123.45)
+        XCTAssertEqual(object?.longitude, -123.45)
+        XCTAssertEqual(object?.radius, 50)
+        XCTAssertEqual(object?.units, .km)
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildHashtag() {
+        let object = try? ActivityContent.Builders.Tags.Hashtag()
+            .setName("#hashtag")
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.name, "#hashtag")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildMention() {
+        let object = try? ActivityContent.Builders.Tags.Mention()
+            .setName("Mention Name")
+            .setDSNPUserId("dsnp://1234")
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.type, "Mention")
+        XCTAssertEqual(object?.name, "Mention Name")
+        XCTAssertEqual(object?.id, "dsnp://1234")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildNote() {
+        let object = try? ActivityContent.Builders.Note()
+            .setContent("Note Content")
+            .setName("Note Name")
+            .setPublished(Date(timeIntervalSince1970: 1640321788.6924329))
+            .addAttachments([
+                try! ActivityContent.Builders.Attachments.Audio()
+                    .addAudioLinks([
+                        try! ActivityContent.Builders.Attachments.AudioLink()
+                            .setHref(URL(string: "https://www.example.com")!)
+                            .setMediaType("audio/ogg")
+                            .addHashes([
+                                try! ActivityContent.Builders.Hash()
+                                    .setAlgorithm("keccak")
+                                    .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                                    .build()
+                            ])
+                            .build()
+                    ])
+                    .build(),
+                try! ActivityContent.Builders.Attachments.Image()
+                    .addImageLinks([
+                        try! ActivityContent.Builders.Attachments.ImageLink()
+                            .setHref(URL(string: "https://www.example.com")!)
+                            .setMediaType("image/png")
+                            .addHashes([
+                                try! ActivityContent.Builders.Hash()
+                                    .setAlgorithm("keccak")
+                                    .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                                    .build()
+                            ])
+                            .build()
+                    ])
+                    .build(),
+                try! ActivityContent.Builders.Attachments.Video()
+                    .addVideoLinks([
+                        try! ActivityContent.Builders.Attachments.VideoLink()
+                            .setHref(URL(string: "https://www.example.com")!)
+                            .setMediaType("video/H265")
+                            .addHashes([
+                                try! ActivityContent.Builders.Hash()
+                                    .setAlgorithm("keccak")
+                                    .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                                    .build()
+                            ])
+                            .build()
+                    ])
+                    .build(),
+                try! ActivityContent.Builders.Attachments.Link()
+                    .setName("Link Attachment")
+                    .setHref(URL(string: "https://www.example.com")!)
+                    .build()
+            ])
+            .addTags([
+                try! ActivityContent.Builders.Tags.Hashtag()
+                    .setName("#hashtag")
+                    .build(),
+                try! ActivityContent.Builders.Tags.Mention()
+                    .setDSNPUserId("dsnp://1234")
+                    .build()
+            ])
+            .setLocation(try! ActivityContent.Builders.Location()
+                            .setName("Location Name")
+                            .build())
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.context, "https://www.w3.org/ns/activitystreams")
+        XCTAssertEqual(object?.type, "Note")
+        XCTAssertEqual(object?.mediaType, "text/plain")
+        XCTAssertEqual(object?.name, "Note Name")
+        XCTAssertEqual(object?.content, "Note Content")
+        XCTAssertEqual(object?.published?.timeIntervalSince1970, 1640321788.6924329)
+        XCTAssertEqual(object?.location?.name, "Location Name")
+        XCTAssertEqual(object?.tag?.count, 2)
+        XCTAssertEqual((object?.tag?[0] as? ActivityContentHashtag)?.name, "#hashtag")
+        XCTAssertEqual((object?.tag?[1] as? ActivityContentMention)?.id, "dsnp://1234")
+        XCTAssertEqual(object?.attachment?.count, 4)
+        XCTAssertEqual((object?.attachment?[0] as? ActivityContentAudioAttachment)?.url?.first?.mediaType, "audio/ogg")
+        XCTAssertEqual((object?.attachment?[1] as? ActivityContentImageAttachment)?.url?.first?.mediaType, "image/png")
+        XCTAssertEqual((object?.attachment?[2] as? ActivityContentVideoAttachment)?.url?.first?.mediaType, "video/H265")
+        XCTAssertEqual((object?.attachment?[3] as? ActivityContentLinkAttachment)?.href?.absoluteString, "https://www.example.com")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildProfile() {
+        let object = try? ActivityContent.Builders.Profile()
+            .setName("Profile Name")
+            .addIcons([
+                try! ActivityContent.Builders.Attachments.ImageLink()
+                    .setHref(URL(string: "https://www.example.com")!)
+                    .setMediaType("image/png")
+                    .addHashes([
+                        try! ActivityContent.Builders.Hash()
+                            .setAlgorithm("keccak")
+                            .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                            .build()
+                    ])
+                    .build()
+            ])
+            .setSummary("Profile Summary")
+            .setPublished(Date(timeIntervalSince1970: 1640321788.6924329))
+            .setLocation(try! ActivityContent.Builders.Location()
+                            .setName("Location Name")
+                            .build())
+            .addTags([
+                try! ActivityContent.Builders.Tags.Hashtag()
+                    .setName("#hashtag")
+                    .build(),
+                try! ActivityContent.Builders.Tags.Mention()
+                    .setDSNPUserId("dsnp://1234")
+                    .build()
+            ])
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.context, "https://www.w3.org/ns/activitystreams")
+        XCTAssertEqual(object?.type, "Profile")
+        XCTAssertEqual(object?.name, "Profile Name")
+        XCTAssertEqual(object?.icon?.count, 1)
+        XCTAssertEqual(object?.icon?[0].mediaType, "image/png")
+        XCTAssertEqual(object?.summary, "Profile Summary")
+        XCTAssertEqual(object?.published?.timeIntervalSince1970, 1640321788.6924329)
+        XCTAssertEqual(object?.location?.name, "Location Name")
+        XCTAssertEqual(object?.tag?.count, 2)
+        XCTAssertEqual((object?.tag?[0] as? ActivityContentHashtag)?.name, "#hashtag")
+        XCTAssertEqual((object?.tag?[1] as? ActivityContentMention)?.id, "dsnp://1234")
         XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
     }
 }
--- Swift Package/Example/Example/ViewController.swift
+++ Swift Package/Example/Example/ViewController.swift
@@ -14,51 +14,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.buildNote()
-        // self.buildProfile()
-        // self.buildAttachments()
+//        self.buildNote()
+//         self.buildProfile()
+         self.buildAttachments()
     }
     
     private func buildNote() {
         do {
             
-            let hashUnsupported = try ActivityContent.HashBuilder()
+            let hashUnsupported = try ActivityContent.Builders.Hash()
                 .setAlgorithm("keccak256")
                 .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
                 .build()
             
-            let hashSupported = try ActivityContent.HashBuilder()
+            let hashSupported = try ActivityContent.Builders.Hash()
                 .setAlgorithm("keccak")
                 .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
                 .build()
             
-            let imageLinkUnsupported = try ActivityContent.ImageLinkBuilder()
+            let imageLinkUnsupported = try ActivityContent.Builders.Attachments.ImageLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("image/fake")
                 .addHashes([hashUnsupported, hashSupported])
                 .build()
             
-            let imageLinkSupported = try ActivityContent.ImageLinkBuilder()
+            let imageLinkSupported = try ActivityContent.Builders.Attachments.ImageLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("image/png")
                 .addHashes([hashUnsupported, hashSupported])
                 .build()
             
-            let imageAttachment = try ActivityContent.ImageAttachmentBuilder()
+            let imageAttachment = try ActivityContent.Builders.Attachments.Image()
                 .setName("Image Attachment")
                 .addImageLinks([imageLinkUnsupported, imageLinkSupported])
                 .build()
             
-            let hashtag = try ActivityContent.HashtagBuilder()
+            let hashtag = try ActivityContent.Builders.Tags.Hashtag()
                 .setName("#hashtag")
                 .build()
             
-            let mention = try ActivityContent.MentionBuilder()
+            let mention = try ActivityContent.Builders.Tags.Mention()
                 .setName("Mention Name")
                 .setDSNPUserId("dsnp://1234")
                 .build()
             
-            let location = try ActivityContent.LocationBuilder()
+            let location = try ActivityContent.Builders.Location()
                 .setName("Location Name")
                 .setAccuracy(50)
                 .setAltitude(100)
@@ -71,12 +71,12 @@ class ViewController: UIViewController {
                 "content" : "override", // ignored
                 "customBool" : true,
                 "customString" : "hello",
-                "customLink" : try ActivityContent.LinkAttachmentBuilder()
+                "customLink" : try ActivityContent.Builders.Attachments.Link()
                     .setHref(URL(string: "http://www.example.com")!)
                     .build()
             ]
             
-            let note = try ActivityContent.NoteBuilder()
+            let note = try ActivityContent.Builders.Note()
                 .setContent("Note Content")
                 .setName("Note Name")
                 .setPublished(Date.now)
@@ -96,29 +96,29 @@ class ViewController: UIViewController {
     private func buildProfile() {
         do {
             
-            let hashUnsupported = try ActivityContent.HashBuilder()
+            let hashUnsupported = try ActivityContent.Builders.Hash()
                 .setAlgorithm("keccak256")
                 .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
                 .build()
             
-            let hashSupported = try ActivityContent.HashBuilder()
+            let hashSupported = try ActivityContent.Builders.Hash()
                 .setAlgorithm("keccak")
                 .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
                 .build()
             
-            let imageLinkUnsupported = try ActivityContent.ImageLinkBuilder()
+            let imageLinkUnsupported = try ActivityContent.Builders.Attachments.ImageLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("image/fake")
                 .addHashes([hashUnsupported, hashSupported])
                 .build()
             
-            let imageLinkSupported = try ActivityContent.ImageLinkBuilder()
+            let imageLinkSupported = try ActivityContent.Builders.Attachments.ImageLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("image/png")
                 .addHashes([hashUnsupported, hashSupported])
                 .build()
             
-            let location = try ActivityContent.LocationBuilder()
+            let location = try ActivityContent.Builders.Location()
                 .setName("Location Name")
                 .setAccuracy(50)
                 .setAltitude(100)
@@ -127,16 +127,16 @@ class ViewController: UIViewController {
                 .setUnits(.cm)
                 .build()
             
-            let hashtag = try ActivityContent.HashtagBuilder()
+            let hashtag = try ActivityContent.Builders.Tags.Hashtag()
                 .setName("#hashtag")
                 .build()
             
-            let mention = try ActivityContent.MentionBuilder()
+            let mention = try ActivityContent.Builders.Tags.Mention()
                 .setName("Mention Name")
                 .setDSNPUserId("dsnp://1234")
                 .build()
             
-            let profile = try ActivityContent.ProfileBuilder()
+            let profile = try ActivityContent.Builders.Profile()
                 .setName("Profile Name")
                 .addIcons([imageLinkUnsupported, imageLinkSupported])
                 .setSummary("Profile Summary")
@@ -155,47 +155,47 @@ class ViewController: UIViewController {
     private func buildAttachments() {
         do {
             
-            let hashSupported = try ActivityContent.HashBuilder()
+            let hashSupported = try ActivityContent.Builders.Hash()
                 .setAlgorithm("keccak")
                 .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
                 .build()
             
-            let imageLinkSupported = try ActivityContent.ImageLinkBuilder()
+            let imageLinkSupported = try ActivityContent.Builders.Attachments.ImageLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("image/png")
                 .addHashes([hashSupported])
                 .build()
             
-            let imageAttachment = try ActivityContent.ImageAttachmentBuilder()
+            let imageAttachment = try ActivityContent.Builders.Attachments.Image()
                 .setName("Image Attachment")
                 .addImageLinks([imageLinkSupported])
                 .build()
             
-            let videoLinkSupported = try ActivityContent.VideoLinkBuilder()
+            let videoLinkSupported = try ActivityContent.Builders.Attachments.VideoLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("video/mp4")
                 .addHashes([hashSupported])
                 .build()
             
-            let videoAttachment = try ActivityContent.VideoAttachmentBuilder()
+            let videoAttachment = try ActivityContent.Builders.Attachments.Video()
                 .setName("Video Attachment")
                 .addDuration(180)
                 .addVideoLinks([videoLinkSupported])
                 .build()
             
-            let audioLinkSupported = try ActivityContent.AudioLinkBuilder()
+            let audioLinkSupported = try ActivityContent.Builders.Attachments.AudioLink()
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .setMediaType("audio/mpeg")
                 .addHashes([hashSupported])
                 .build()
             
-            let audioAttachment = try ActivityContent.AudioAttachmentBuilder()
+            let audioAttachment = try ActivityContent.Builders.Attachments.Audio()
                 .setName("Audio Attachment")
                 .addDuration(240)
                 .addAudioLinks([audioLinkSupported])
                 .build()
             
-            let linkAttachment = try ActivityContent.LinkAttachmentBuilder()
+            let linkAttachment = try ActivityContent.Builders.Attachments.Link()
                 .setName("Link Attachment")
                 .setHref(URL(string: "http://www.example.com/image.png")!)
                 .build()
@@ -207,7 +207,7 @@ class ViewController: UIViewController {
                 linkAttachment
             ]
             
-            let note = try ActivityContent.NoteBuilder()
+            let note = try ActivityContent.Builders.Note()
                 .setContent("Note Content")
                 .addAttachments(attachments)
                 .build()

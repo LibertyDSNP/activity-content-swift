d0d3d90aa25502a9095702d4f028f90c0894ced4
Rigo Carbajal <rigo.carbajal@unfinished.com>
2022-01-14 16:18:17 -0800

    Wrote tests for Attachments Builder classes.

--- Swift Package/ActivityContentSDK/Tests/ActivityContentSDKTests/Source/ActivityContentTests.swift
+++ Swift Package/ActivityContentSDK/Tests/ActivityContentSDKTests/Source/ActivityContentTests.swift
@@ -76,4 +76,101 @@ class ActivityContentTests: XCTestCase {
         XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
     }
     
+    func testBuildImageAttachment() {
+        let object = try? ActivityContent.Builders.Attachments.Image()
+            .setName("Image Attachment")
+            .addImageLinks([
+                try! ActivityContent.Builders.Attachments.ImageLink()
+                    .setHref(URL(string: "https://www.example.com")!)
+                    .setMediaType("image/png")
+                    .addHashes([
+                        try! ActivityContent.Builders.Hash()
+                            .setAlgorithm("keccak")
+                            .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                            .build()
+                    ])
+                    .build()
+            ])
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.name, "Image Attachment")
+        XCTAssertEqual(object?.url?.first?.mediaType, "image/png")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildImageLink() {
+        let object = try? ActivityContent.Builders.Attachments.ImageLink()
+            .setHref(URL(string: "https://www.example.com")!)
+            .setMediaType("image/png")
+            .addHashes([
+                try! ActivityContent.Builders.Hash()
+                    .setAlgorithm("keccak")
+                    .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                    .build()
+            ])
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
+        XCTAssertEqual(object?.hash?.first?.algorithm, "keccak")
+        XCTAssertEqual(object?.mediaType, "image/png")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildVideoAttachment() {
+        let object = try? ActivityContent.Builders.Attachments.Video()
+            .setName("Video Attachment")
+            .addVideoLinks([
+                try! ActivityContent.Builders.Attachments.VideoLink()
+                    .setHref(URL(string: "https://www.example.com")!)
+                    .setMediaType("video/H265")
+                    .addHashes([
+                        try! ActivityContent.Builders.Hash()
+                            .setAlgorithm("keccak")
+                            .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                            .build()
+                    ])
+                    .build()
+            ])
+            .addDuration(180)
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.name, "Video Attachment")
+        XCTAssertEqual(object?.duration, 180)
+        XCTAssertEqual(object?.url?.first?.mediaType, "video/H265")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildVideoLink() {
+        let object = try? ActivityContent.Builders.Attachments.VideoLink()
+            .setHref(URL(string: "https://www.example.com")!)
+            .setMediaType("video/H265")
+            .addHashes([
+                try! ActivityContent.Builders.Hash()
+                    .setAlgorithm("keccak")
+                    .setValue("0x00a63eb58f6ce7fccd93e2d004fed81da5ec1a9747b63f5f1bf80742026efea7")
+                    .build()
+            ])
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
+        XCTAssertEqual(object?.hash?.first?.algorithm, "keccak")
+        XCTAssertEqual(object?.mediaType, "video/H265")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
+    
+    func testBuildLinkAttachment() {
+        let object = try? ActivityContent.Builders.Attachments.Link()
+            .setName("Link Attachment")
+            .setHref(URL(string: "https://www.example.com")!)
+            .addAdditionalFields(["custom" : true])
+            .build()
+        
+        XCTAssertEqual(object?.name, "Link Attachment")
+        XCTAssertEqual(object?.href?.absoluteString, "https://www.example.com")
+        XCTAssertEqual(object?.additionalFields?["custom"] as? Bool, true)
+    }
 }
